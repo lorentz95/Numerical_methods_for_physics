cd8610d37f04454b9e35f40de0a76f69b7b07f31
Lorenzo <161303222+lorentz95@users.noreply.github.com>
2024-03-10 14:10:15 +0100

    Update N-N_in_raw_Python.py
    Loss Implemented and correct structure of file

--- N-N_in_raw_Python.py
+++ N-N_in_raw_Python.py
@@ -1,26 +1,17 @@
-import numpy as np
-
-class Layer:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
-
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
-
-class Activation_ReLU:
-    def forward(self, inputs):
-        self.output = np.maximum(0, inputs)
-
+# First import classes along with numpy
+from NN_classes import *
 
 np.random.seed(0)
 
 # Initialize inputs
-initial_inputs = np.array([[1, 2, 3, 2.5], [2, 1, 5, 9.5]])
+initial_inputs = np.array([[4.8, 1.21, 2.385], [8.9, -1.81, 0.2], [1.41, 1.051, 0.026]])
+
+
+################################################################
 
 ### N-N with 2 hidden layers
 # Create first layer
-layer0 = Layer(4, 5)
+layer0 = Layer(3, 5)
 
 # Create first hidden layer
 layer1 = Layer(5, 6)
@@ -31,14 +22,22 @@ layer2 = Layer(6, 4)
 # Create final layer
 layer3 = Layer(4, 2)
 
-# Activation Functions (Rectified Linear, i.e. 0 if x<0 or x if x>0)
+################################################################
+
+### ACTIVATION FUNCTIONS
+
+# Rectified Linear Units (ReLU), i.e. 0 if x<0 or x if x>0
 activation0 = Activation_ReLU()
 activation1 = Activation_ReLU()
 activation2 = Activation_ReLU()
-activation3 = Activation_ReLU()
+
+# Softmax, i.e. exponentiates and normalizes last neuron's inputs
+activation3 = Activation_Softmax()
 
 
-# Activate N-N
+################################################################
+
+# ACTIVATE N-N
 layer0.forward(initial_inputs)
 activation0.forward(layer0.output)
 
@@ -51,5 +50,15 @@ activation2.forward(layer2.output)
 layer3.forward(activation2.output)
 activation3.forward(layer3.output)
 
-print(activation3.output)
+print("Results:\n", activation3.output)
 
+
+################################################################
+# EVALUATING THE LOSS
+
+# Target values
+y = np.array([0, 0, 1])
+
+loss_function = Loss_CategoricalCrossEntropy()
+loss = loss_function.calculate(activation3.output, y)
+print("\nLoss: ", loss)
