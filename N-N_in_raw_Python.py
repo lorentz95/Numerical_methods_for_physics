13bd165773cb26dc9c2624e2f47c9f0989192f5e
Lorenzo <161303222+lorentz95@users.noreply.github.com>
2024-03-10 10:58:02 +0100

    Update N-N_in_raw_Python.py
    Activation function Relu implemented

--- N-N_in_raw_Python.py
+++ N-N_in_raw_Python.py
@@ -8,11 +8,18 @@ class Layer:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+np.random.seed(0)
+
 # Initialize inputs
-initial_inputs = np.array([[1, 2, 3, 2.5]])
+initial_inputs = np.array([[1, 2, 3, 2.5], [2, 1, 5, 9.5]])
 
 ### N-N with 2 hidden layers
-# Create first layers
+# Create first layer
 layer0 = Layer(4, 5)
 
 # Create first hidden layer
@@ -24,11 +31,25 @@ layer2 = Layer(6, 4)
 # Create final layer
 layer3 = Layer(4, 2)
 
+# Activation Functions (Rectified Linear, i.e. 0 if x<0 or x if x>0)
+activation0 = Activation_ReLU()
+activation1 = Activation_ReLU()
+activation2 = Activation_ReLU()
+activation3 = Activation_ReLU()
+
+
 # Activate N-N
 layer0.forward(initial_inputs)
-layer1.forward(layer0.output)
-layer2.forward(layer1.output)
-layer3.forward(layer2.output)
+activation0.forward(layer0.output)
 
-print(layer3.output)
+layer1.forward(activation0.output)
+activation1.forward(layer1.output)
+
+layer2.forward(activation1.output)
+activation2.forward(layer2.output)
+
+layer3.forward(activation2.output)
+activation3.forward(layer3.output)
+
+print(activation3.output)
 
