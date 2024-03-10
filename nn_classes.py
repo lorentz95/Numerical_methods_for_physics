782b5a11248e484ff371ab714a277d3c136b249a
Lorenzo <161303222+lorentz95@users.noreply.github.com>
2024-03-10 14:29:08 +0100

    Update nn_classes.py
    Randomization of biases

--- nn_classes.py
+++ nn_classes.py
@@ -4,7 +4,7 @@ import numpy as np
 class Layer:
     def __init__(self, n_inputs, n_neurons):
         self.weights = np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
+        self.biases = np.random.randn(1, n_neurons)
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
